a0c96aa1168bf35e5511a43f900ce7b3d53085e1
Simran <simran5590@gmail.com>
2020-12-23 20:40:08 -0800

    Basic setup of Fragment UIs and classes

--- app/src/main/java/app/simran/fragmentsexample/MainActivity.kt
+++ app/src/main/java/app/simran/fragmentsexample/MainActivity.kt
@@ -2,10 +2,25 @@ package app.simran.fragmentsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var btn1: Button = findViewById(R.id.btnFragment)
+    var btn2: Button = findViewById(R.id.btnFragment2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1.setOnClickListener { showFragment1() }
+        btn2.setOnClickListener { showFragment2() }
+    }
+
+    private fun showFragment1() {
+
+    }
+
+    private fun showFragment2() {
+
     }
 }
