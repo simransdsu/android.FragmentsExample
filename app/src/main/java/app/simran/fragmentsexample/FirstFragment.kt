b12ea5f808cf14636313f850e4a958d46451c7db
Simran <simran5590@gmail.com>
2020-12-23 20:41:53 -0800

    much more simpler way to inflate fragment layout for its fragment

--- app/src/main/java/app/simran/fragmentsexample/FirstFragment.kt
+++ app/src/main/java/app/simran/fragmentsexample/FirstFragment.kt
@@ -6,10 +6,5 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FirstFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+class FirstFragment : Fragment(R.layout.fragment_first) {
 }
