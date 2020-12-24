814fdecfe0185b83186c5f34a56351b7be145f54
Simran <simran5590@gmail.com>
2020-12-23 20:53:12 -0800

    Showing fragment programatically

--- app/src/main/java/app/simran/fragmentsexample/MainActivity.kt
+++ app/src/main/java/app/simran/fragmentsexample/MainActivity.kt
@@ -6,21 +6,36 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    var btn1: Button = findViewById(R.id.btnFragment)
-    var btn2: Button = findViewById(R.id.btnFragment2)
+    lateinit var btn1: Button
+    lateinit var btn2: Button
+
+    val firstFragment = FirstFragment()
+    val secondFragment = SecondFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1 = findViewById(R.id.btnFragment)
+        btn2 = findViewById(R.id.btnFragment2)
+
         btn1.setOnClickListener { showFragment1() }
         btn2.setOnClickListener { showFragment2() }
+
+        showFragment1()
     }
 
     private fun showFragment1() {
-
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, firstFragment)
+            commit()
+        }
     }
 
     private fun showFragment2() {
-
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, secondFragment)
+            commit()
+        }
     }
 }
